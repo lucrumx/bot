8e67e66ce6795015a744d39d10e75c7fdf29493a
Igor Deych <hurgadan@gmail.com>
2026-01-19 12:04:48 +0100

    feat: add JWT auth, tests

--- internal/auth/jwt_test.go
+++ internal/auth/jwt_test.go
@@ -9,15 +9,43 @@ import (
 // TestValidateJWT tests the generation and validation of a JWT, ensuring valid claims and no errors during the process.
 func TestValidateJWT(t *testing.T) {
 	t.Setenv("JWT_SECRET", "some-secret-key")
-
 	userID := uint(123)
-	token, err := GenerateJWT(userID)
+	validToken, _ := GenerateJWT(userID)
 
-	assert.NoError(t, err)
-	assert.NotEmpty(t, token)
+	tests := []struct {
+		name        string
+		tokenString string
+		wantErr     bool
+		wantUserID  uint
+	}{
+		{
+			name:        "Valid token",
+			tokenString: validToken,
+			wantErr:     false,
+			wantUserID:  userID,
+		},
+		{
+			name:        "Invalidate token",
+			tokenString: "invalid-token-string",
+			wantErr:     true,
+		},
+		{
+			name:        "Empty token",
+			tokenString: "",
+			wantErr:     true,
+		},
+	}
 
-	claims, err := ValidateJWT(token)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			claims, err := ValidateJWT(tt.tokenString)
 
-	assert.NoError(t, err)
-	assert.Equal(t, userID, claims.Sub)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.wantUserID, claims.Sub)
+			}
+		})
+	}
 }
