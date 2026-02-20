695a5104d4e5ff71513e35704711c6c09b4ebb5a
Igor Deych <hurgadan@gmail.com>
2026-02-20 01:46:20 +0100

    feat: Arbitrage executor (orders)

---
+++ internal/exchange/client/bingx/get-balances.go
@@ -0,0 +1,12 @@
+package bingx
+
+import (
+	"context"
+
+	"github.com/lucrumx/bot/internal/models"
+)
+
+// GetBalances returns the balances of the user's account on the exchange.
+func (c *Client) GetBalances(ctx context.Context) ([]models.Balance, error) {
+	return nil, nil
+}
---
+++ internal/exchange/client/bybit/create-order.go
@@ -0,0 +1,8 @@
+package bybit
+
+import "github.com/lucrumx/bot/internal/models"
+
+// CreateOrder creates an order on the exchange.
+func (c *Client) CreateOrder(ctx, req models.Order) (*models.Order, error) {
+	return nil, nil
+}
